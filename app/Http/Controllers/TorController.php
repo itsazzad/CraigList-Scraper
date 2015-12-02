0da1b732a37d2f92e34f194d211d26c3d7627f00
Sazzad Hossain Khan <itsazzad@gmail.com>
2015-12-02 14:30:14 +0600

    Create TorController.php

---
+++ app/Http/Controllers/TorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use App\Link;
+use App\Scrap;
+use Goutte\Client;
+use Illuminate\Http\Request;
+
+class TorController extends Controller
+{
+    
+
+    public function getIndex()
+    {
+
+
+    }
+
+    public function getData(){
+
+    }
+
+}
