c5513720fa8689c20dc26adddce83859e930b9cf
Sazzad Hossain Khan <itsazzad@gmail.com>
2015-12-02 14:32:20 +0600

    Update TorController.php

--- app/Http/Controllers/TorController.php
+++ app/Http/Controllers/TorController.php
@@ -15,6 +15,28 @@ class TorController extends Controller
 
     public function getIndex()
     {
+        // Connect to the TOR server using password authentication
+        $tc = new \TorControl\TorControl(
+            array(
+                'server' => 'localhost',
+                'port'   => 9051,
+                'password' => '16:0DB96B1B985D6BA160DEE39AB7FCCBDFB864CE6C89FF8DCB63982F1AC2',//0VTbMcmjTi
+                'authmethod' => 1
+            )
+        );
+        
+        $tc->connect();
+        
+        $tc->authenticate();
+        
+        // Renew identity
+        $res = $tc->executeCommand('SIGNAL NEWNYM');
+        
+        // Echo the server reply code and message
+        echo $res[0]['code'].': '.$res[0]['message'];
+        
+        // Quit
+        $tc->quit();
 
 
     }
